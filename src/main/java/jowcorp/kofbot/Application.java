9294a236c40b1d2f24006078c72eeeaf0f348054
jowred <jonathanalencar97@gmail.com>
2022-02-16 20:47:05 -0300

    Adicionando mensagem na tela e mudanças no application.properties

--- src/main/java/jowcorp/kofbot/Application.java
+++ src/main/java/jowcorp/kofbot/Application.java
@@ -2,14 +2,24 @@ package jowcorp.kofbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import jowcorp.kofbot.bots.KofBot;
 
+@Controller
 @SpringBootApplication
 public class Application {
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String home() {
+		return "O KofBot está no ar!";
+	}
 
 	public static void main(String[] args) {
 		try {
